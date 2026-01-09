c19a2df089ab08c937bc9e9e30806359d5ade5a1
AliakseiLuu <aliaksei.lupachyk@gmail.com>
2026-01-09 17:45:52 +0300

    lecture_5.2 Добавлен тест DashboardElementsTest

--- src/main/java/pages/Dashboard.java
+++ src/main/java/pages/Dashboard.java
@@ -7,6 +7,15 @@ import org.openqa.selenium.WebDriver;
 public class Dashboard extends BasePage {
 
   private final By dashboardHeader = By.xpath("//span/h6");
+  private final By timeAtWorkSheet = By.xpath("//div[1]/div[@class='oxd-sheet oxd-sheet--rounded oxd-sheet--white orangehrm-dashboard-widget']");
+  private final By myActionsSheet = By.xpath("//div[2]/div[@class='oxd-sheet oxd-sheet--rounded oxd-sheet--white orangehrm-dashboard-widget']");
+  private final By quickLaunchSheet = By.xpath("//div[3]/div[@class='oxd-sheet oxd-sheet--rounded oxd-sheet--white orangehrm-dashboard-widget']");
+  private final By buzzLatestPostsSheet = By.xpath("//div[4]/div[@class='oxd-sheet oxd-sheet--rounded oxd-sheet--white orangehrm-dashboard-widget']");
+  private final By employeesOnLeaveTodaySheet = By.xpath("//div/div[@class='oxd-sheet oxd-sheet--rounded oxd-sheet--white orangehrm-dashboard-widget emp-leave-chart']");
+  private final By employeeDistributionBySubUnitSheet = By.xpath("//div[6]/div[@class='oxd-sheet oxd-sheet--rounded oxd-sheet--white orangehrm-dashboard-widget']");
+  private final By employeeDistributionByLocationSheet = By.xpath("//div[7]/div[@class='oxd-sheet oxd-sheet--rounded oxd-sheet--white orangehrm-dashboard-widget']");
+
+
 
   public Dashboard(final WebDriver driver) {
     super(driver);
@@ -34,11 +43,35 @@ public class Dashboard extends BasePage {
   }
 
   public boolean isHeaderVisible() {
-    try {
-      waitForVisibility(dashboardHeader);
-      return true;
-    } catch (TimeoutException e) {
-      return false;
-    }
+    return isDisplayed(dashboardHeader);
   }
+
+  public boolean isTimeAtWorkSheetVisible() {
+    return isDisplayed(timeAtWorkSheet);
+  }
+
+  public boolean isMyActionsSheetVisible() {
+    return isDisplayed(myActionsSheet);
+  }
+
+  public boolean isQuickLaunchSheetVisible() {
+    return isDisplayed(quickLaunchSheet);
+  }
+
+  public boolean isBuzzLatestPostsSheetVisible() {
+    return isDisplayed(buzzLatestPostsSheet);
+  }
+
+  public boolean isEmployeesOnLeaveTodaySheetVisible() {
+    return isDisplayed(employeesOnLeaveTodaySheet);
+  }
+
+  public boolean isEmployeeDistributionBySubUnitSheetVisible() {
+    return isDisplayed(employeeDistributionBySubUnitSheet);
+  }
+
+  public boolean isEmployeeDistributionByLocationSheetVisible() {
+    return isDisplayed(employeeDistributionByLocationSheet);
+  }
+
 }
