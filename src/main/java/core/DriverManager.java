cd5dffa1b4de9f58b944c8fc7bfff7e2ab1a9d07
a.lupachyk <aliaksei_lupachyk@senla.eu>
2026-09-21 20:08:47 +0300

    lecture_6 fix checkstyle error

--- src/main/java/core/DriverManager.java
+++ src/main/java/core/DriverManager.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverManager {
 
-  private static final ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+  private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
   private DriverManager() {}
 
